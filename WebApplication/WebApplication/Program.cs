8e63a3bf727b4a5c8da01d130a496fd6fe40956a
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-10-08 23:21:15 -0300

    Ajustando migrations

--- WebApplication/WebApplication/Program.cs
+++ WebApplication/WebApplication/Program.cs
@@ -23,6 +23,12 @@ builder.Services.AddScoped<IPessoaRepository, PessoaRepositoryImplementation>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+	var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+	db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
