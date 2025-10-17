d6996d36774104fa1a2221ab62a1305bf4ab2773
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-10-16 21:46:17 -0300

    Custom Serialization

--- WebApplication/WebApplication/Data/VO/LivroVO.cs
+++ WebApplication/WebApplication/Data/VO/LivroVO.cs
@@ -1,11 +1,22 @@
-﻿namespace WebApplication.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace WebApplication.Data.VO
 {
 	public class LivroVO
 	{
+		[JsonPropertyName("Id")]
 		public int Id { get; set; }
+
+		[JsonPropertyName("Title")]
 		public string Titulo { get; set; }
+
+		[JsonPropertyName("Author")]
 		public string Autor { get; set; }
+
+		[JsonPropertyName("Date of birth")]
 		public DateTime DataDeLancamento { get; set; }
+
+		[JsonPropertyName("Price")]
 		public decimal Preco { get; set; }
 	}
 }
--- WebApplication/WebApplication/Data/VO/PessoaVO.cs
+++ WebApplication/WebApplication/Data/VO/PessoaVO.cs
@@ -1,11 +1,22 @@
-﻿namespace WebApplication.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace WebApplication.Data.VO
 {
 	public class PessoaVO
 	{
+		[JsonPropertyName("Id")]
 		public int Id { get; set; }
+
+		[JsonPropertyName("Name")]
 		public string Nome { get; set; }
+		
+		[JsonPropertyName("Last Name")]
 		public string Sobrenome { get; set; }
+
+		[JsonPropertyName("Address")]
 		public string Endereco { get; set; }
+
+		[JsonPropertyName("Sex")]
 		public string Sexo { get; set; }
 	}
 }
