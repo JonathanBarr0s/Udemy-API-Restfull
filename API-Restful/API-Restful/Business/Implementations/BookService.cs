8ae5d861e4df574ac6cc83de0c89e042ee1f7764
Jonathan Barros <jonathan.sbarros@outlook.com.br>
2025-11-02 15:35:45 -0300

    Implementando Mapster para BookDTO

--- API-Restful/API-Restful/Business/Implementations/BookService.cs
+++ API-Restful/API-Restful/Business/Implementations/BookService.cs
@@ -2,44 +2,43 @@
 using API_Restful.Data.Converter.Implementations;
 using API_Restful.Data.Repository.Interfaces;
 using API_Restful.Domain.Entities;
+using Mapster;
 
 namespace API_Restful.Business.Implementations
 {
 	public class BookService : IBookService
 	{
 		private IRepository<Book> _repository;
-		private readonly BookConverter _bookConverter;
 
 		public BookService(IRepository<Book> repository)
 		{
 			_repository = repository;
-			_bookConverter = new BookConverter();
 		}
 
 		public List<BookDTO> FindAll()
 		{
-			return _bookConverter.ParseList(_repository.FindAll());
+			return _repository.FindAll().Adapt<List<BookDTO>>();
 		}
 
 		public BookDTO FindById(int id)
 		{
-			return _bookConverter.Parse(_repository.FindById(id));
+			return _repository.FindById(id).Adapt<BookDTO>();
 		}
 
 		public BookDTO Create(BookDTO bookDTO)
 		{
-			var entity = _bookConverter.Parse(bookDTO);
+			var entity = bookDTO.Adapt<Book>();
 			entity = _repository.Create(entity);
 
-			return _bookConverter.Parse(entity);
+			return entity.Adapt<BookDTO>();
 		}
 
 		public BookDTO Update(BookDTO bookDTO)
 		{
-			var entity = _bookConverter.Parse(bookDTO);
+			var entity = bookDTO.Adapt<Book>();
 			entity = _repository.Update(entity);
 
-			return _bookConverter.Parse(entity);
+			return entity.Adapt<BookDTO>();
 		}
 		public void Delete(int id)
 		{
